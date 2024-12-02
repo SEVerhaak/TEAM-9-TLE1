b37e20f4a33edc53e0feb59c95705d0a8858c168
basverhaak <severhaak@hotmail.nl>
2024-12-02 14:37:01 +0100

    migration

---
+++ database/migrations/2024_12_02_131807_create_user_jobs_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up()
+    {
+        Schema::create('user_jobs', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained()->onDelete('cascade'); // Links to users
+            $table->foreignId('job_id')->constrained()->onDelete('cascade'); // Links to jobs
+            $table->integer('application_stage')->default(0); // 0: applied, 1: interview, etc.
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('user_jobs');
+    }
+};
