d964946a191e3acf266119477499fb16304a2404
Thijs Kraaijenbos <47988622+ThijsKraaijenbos@users.noreply.github.com>
2024-12-03 12:48:16 +0100

    Openstaande vacatures en detailspagina van vacatures basis

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\VacancyController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -16,5 +17,9 @@ Route::middleware('auth')->group(function () {
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
+
+
+Route::resource('open_vacancies', VacancyController::class)->names('open_vacancies');
+
 
 require __DIR__.'/auth.php';
