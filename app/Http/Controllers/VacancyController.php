af5ed33ef7321f2fd1d4c46885ec29ff0fbe74ae
Vincent Benders <74290916+VincentBenders@users.noreply.github.com>
2024-12-03 11:58:00 +0100

    made function to show vacanties on personal dashboard

---
+++ app/Http/Controllers/VacancyController.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\UserVacancy;
+use App\Models\Vacancy;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class VacancyController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Vacancy $vacancy)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Vacancy $vacancy)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Vacancy $vacancy)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Vacancy $vacancy)
+    {
+        //
+    }
+    public function dashboard() {
+        $userId = Auth::id();
+
+        $vacancies = UserVacancy::where('user_id', $userId)->orderBy('created_at', 'desc')->get();
+        return view('dashboard', compact('vacancies'));
+    }
+}
--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -9,7 +9,17 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900 dark:text-gray-100">
-                    {{ __("You're logged in!") }}
+                    @foreach($vacancies as $vacancy)
+                        @dump($vacancies)
+                        <div>
+                           <p> {{$vacancy}}</p>
+                            <p> {{$vacancy->vacancy->name}}</p>
+                            <p> {{$vacancy->vacancy->salary}}</p>
+                            <p> {{$vacancy->vacancy->time_hours}}</p>
+                            <p> {{$vacancy->vacancy->image}}</p>
+                            <button class="btn">cancel</button>
+                        </div>
+                    @endforeach
                 </div>
             </div>
         </div>
