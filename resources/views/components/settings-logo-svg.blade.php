eb3d93afb9697b598ac7f437b4bd0c497851db71
basverhaak <severhaak@hotmail.nl>
2024-12-04 13:30:22 +0100

    Vacature overzicht pagina is af. Icoontjes toegevoegd

---
+++ resources/views/components/settings-logo-svg.blade.php
@@ -0,0 +1,18 @@
+<svg xmlns="http://www.w3.org/2000/svg" width="20%" height="20%" viewBox="0 0 28.063 28.063">
+    <defs>
+        <style>
+            .cls-1 {
+                fill: none;
+                stroke: #fff;
+                stroke-linecap: round;
+                stroke-width: 3px;
+            }
+        </style>
+    </defs>
+    <g id="Icon_akar-settings-horizontal" data-name="Icon akar-settings-horizontal" transform="translate(-3 -3)">
+        <path id="Path_18" data-name="Path 18" class="cls-1" d="M4.5,7.5h5.57m19.493,0H15.639M4.5,17.247H21.209m8.354,0H26.778M4.5,26.993H7.285m22.278,0H12.854" transform="translate(0 -0.215)"/>
+        <path id="Path_19" data-name="Path 19" class="cls-1" d="M16.07,7.285A2.785,2.785,0,1,1,13.285,4.5,2.785,2.785,0,0,1,16.07,7.285Z" transform="translate(-0.43)"/>
+        <path id="Path_20" data-name="Path 20" class="cls-1" d="M28.07,17.785A2.785,2.785,0,1,1,25.285,15,2.785,2.785,0,0,1,28.07,17.785Z" transform="translate(-1.291 -0.753)"/>
+        <path id="Path_21" data-name="Path 21" class="cls-1" d="M13.07,28.285A2.785,2.785,0,1,1,10.285,25.5,2.785,2.785,0,0,1,13.07,28.285Z" transform="translate(-0.215 -1.507)"/>
+    </g>
+</svg>
--- resources/views/open_vacancies.blade.php
+++ resources/views/open_vacancies.blade.php
@@ -1,39 +1,173 @@
-<x-template></x-template>
-{{--Message die je krijgt als je voor een vacature hebt aangemeld of uitgeschreven--}}
-@if (session('message'))
-    <div>
-        <h1>{{ session('message') }}</h1>
-    </div>
-@endif
-<h1>Openstaande Vacatures</h1>
+<x-template>
 
-{{-- Search filter formulier. WERKT NU NOG NIET OMDAT ER GEEN USER STORY VOOR WAS!--}}
-{{-- Willen we dit async maken? --}}
-{{--   Action = {{ route('open_vacancies.search')}}--}}
-<form method="GET" action="">
-{{--Zet deze op hidden--}}
-    <label for="search_vacancy_input">Search</label>
-    <input name="search_vacancy_input" placeholder="Search">
-    <div>
-        <button>
-{{--            <img src="">--}}
-            Filters
-        </button>
-        <button>
-{{--            <img src="">--}}
-            Sort by
-        </button>
-    </div>
-</form>
+</x-template>
+<div class="wrapper">
+    {{--Message die je krijgt als je voor een vacature hebt aangemeld of uitgeschreven--}}
+    @if (session('message'))
+        <div>
+            <h1>{{ session('message') }}</h1>
+        </div>
+    @endif
+    <h2 style="text-align: center">Openstaande Vacatures</h2>
 
-@foreach($vacancies as $vacancy)
-    <div>
-        <h1>{{$vacancy->name}}</h1>
-        <h2>{{$vacancy->business->name}}</h2>
-        <h2>{{$vacancy->business->hq_location}}</h2>
-        <h2>{{$vacancy->time_hours}} Hours per week</h2>
-        <h2>{{$vacancy->salary}} Euro (Gross income per month)</h2>
+    {{-- Search filter formulier. WERKT NU NOG NIET OMDAT ER GEEN USER STORY VOOR WAS!--}}
+    {{-- Willen we dit async maken? --}}
+    {{--   Action = {{ route('open_vacancies.search')}}--}}
+    <form method="GET" action="">
+        {{--Zet deze op hidden--}}
+        <label for="search_vacancy_input"></label>
+        <input name="search_vacancy_input" class="search-vacancy" placeholder="Search">
 
-        <a href="{{route('open_vacancies.show', $vacancy->id)}}">More Info</a>
-    </div>
-@endforeach
+        <div class="button-container-2">
+            <button class="filters">
+                <div class="button-flex">
+                    <x-settings-logo-svg>
+
+                    </x-settings-logo-svg>
+                    <p> Filters</p>
+                </div>
+            </button>
+            <button class="sort-by">
+                <div class="button-flex">
+                    <x-sort-by-logo-svg>
+
+                    </x-sort-by-logo-svg>
+                    <p> Sorteer</p>
+                </div>
+            </button>
+        </div>
+    </form>
+
+    @foreach($vacancies as $vacancy)
+        <div class="result-container">
+            <h2>{{$vacancy->name}}</h2>
+            <div class="text-icon-content-container">
+                <x-icon-building-svg>
+
+                </x-icon-building-svg>
+                <p>{{$vacancy->business->name}}</p>
+            </div>
+            <div class="text-icon-content-container">
+                <x-icon-map-svg>
+
+                </x-icon-map-svg>
+                <p>{{$vacancy->business->hq_location}}</p>
+            </div>
+            <div class="text-icon-content-container">
+                <x-icon-clock-svg>
+
+                </x-icon-clock-svg>
+                <p>{{$vacancy->time_hours}} Uur per week</p>
+            </div>
+            <div class="text-icon-content-container">
+                <x-icon-money-svg>
+
+                </x-icon-money-svg>
+                <p>{{$vacancy->salary}} Euro (Per maand)</p>
+            </div>
+
+            <button class="more-info">
+                <div class="button-content-container">
+                    <a href="{{route('open_vacancies.show', $vacancy->id)}}">Meer informatie</a>
+                    <x-icon-info-svg>
+
+                    </x-icon-info-svg>
+                </div>
+            </button>
+        </div>
+    @endforeach
+</div>
+<style>
+
+    .text-icon-content-container{
+        display: flex;
+        align-items: center;
+        justify-content: flex-start;
+        gap: 0.5rem;
+    }
+
+    .result-container {
+        max-width: 90%;
+        background-color: white;
+        box-shadow: 0 0 30px rgba(0, 0, 0, 0.1);
+        padding: 1rem;
+        margin: 2rem 1rem;
+        border-radius: 0 30px 30px 30px;
+    }
+
+    .result-container p {
+        margin: 0.25rem 0;
+        font-size: large;
+    }
+
+    .more-info {
+        width: 100%;
+        background-color: #b4085c;
+        margin: 1rem 1rem 0 0;
+        border: none;
+        color: white;
+        padding: 1rem;
+        border-radius: 0 15px 15px 15px;
+    }
+
+    .more-info a {
+        color: white;
+        font-size: large;
+        font-weight: bold;
+        font-family: "Radikal Trial";
+        text-decoration: none;
+    }
+
+    .filters {
+        background-color: #b4085c;
+        border-radius: 15px;
+        border: none;
+        color: white;
+        font-weight: bold;
+        max-width: 40%;
+        min-height: 3rem;
+        max-height: 3rem;
+        font-size: larger;
+    }
+
+    p {
+        margin: 0;
+    }
+
+    .sort-by {
+        background-color: #f9ee00;
+        border-radius: 15px;
+        border: none;
+        color: black;
+        font-weight: bold;
+        max-width: 40%;
+        min-height: 3rem;
+        max-height: 3rem;
+        font-size: larger;
+    }
+
+    .button-flex {
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        gap: 0.5rem;
+    }
+
+    .button-container-2 {
+        display: flex;
+        flex-direction: row;
+        justify-content: space-between;
+        gap: 2rem;
+        margin: 1rem;
+    }
+
+    .search-vacancy {
+        border-radius: 15px;
+        border: 1px solid black;
+        width: 90%;
+        padding: 1rem 0;
+        text-indent: 2rem;
+        margin: 1rem;
+    }
+
+</style>
